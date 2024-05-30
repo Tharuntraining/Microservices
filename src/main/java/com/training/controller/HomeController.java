98fde62a157b0548ea406853042a792551bae265
Tharun Hanmandla <tharun.hanmandla@cognine.com>
2024-05-30 11:45:01 +0530

    changed return message

--- src/main/java/com/training/controller/HomeController.java
+++ src/main/java/com/training/controller/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-	@GetMapping("getwelcomemessage")
+	@GetMapping("getmoviecatalog")
 	public String getWelcomeMessage() {
-		return "welcome to micro services";
+		return "welcome to movie catalog";
 	}
 
 }
